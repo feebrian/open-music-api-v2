44d811a7a3a5cb7540f7bb0643154bc764be56a1
feebrian <adamaditama13@gmail.com>
2023-12-29 23:44:40 +0700

    implement album services

--- src/server.js
+++ src/server.js
@@ -1,13 +1,64 @@
 require('dotenv').config();
 const hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+
+// albums
+const albums = require('./api/albums');
+const AlbumsService = require('./service/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
   });
 
+  // internal plugin
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (req, h) => {
+    const { response } = req;
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'Terjadi kegagalan pada server kami',
+      });
+
+      newResponse.code(500);
+      console.error(response);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
   await server.start();
+  // eslint-disable-next-line no-console
   console.log(`Server start running on ${server.info.uri}`);
 };
 
